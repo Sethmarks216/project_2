d09eda686678a490b9f2b5a4c85e37abcb38d3c5
Fausnightm <m.fausnight@lafilm.edu>
2021-08-26 20:01:17 -0400

    got rid of routes folde

--- routes/api/index.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const dogRoutes = require ('./dog-routes')
-const userRoutes = require ('./user-routes')
-
-router.use('/dog', dogRoutes)
-router.use('/user', userRoutes)
-
-module.exports = router
